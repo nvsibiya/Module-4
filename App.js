e48c123c99bc6b5e81902fc35f79be2f5b4b4f3a
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2020-06-10 00:31:26 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,49 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './Snippet.jsx';
-
-//CONSTRUCTOR gets 
-//RENDER
-//COMPONENTDIDMOUNT
-//COMPONENTDIDUPDATE
-//COMPONENTWILLUNMOUNT
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import Reservations from './components/Reservations'
+import DishDetail from './components/DishDetail'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
-
-  state = {
-    test:''
-  }
-
-  componentDidMount = async () => {
-    console.log("COMPONENTDIDMOUNT METHOD") //PERFECT FOR ASYNC CALLS
-  }
-
-  buttonClick = () => {
-    this.setState({
-      test: "something",
-      show: !this.state.show
-    })
-  }
-
-  componentDidUpdate = () => {
-    console.log("COMPONENTDIDUPDATE")
-    if (this.state.test !=='something else'){
-      this.setState({
-        test: this.state.test + ' else'
-      })
-    }
-  }
-
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <button type="button" onClick={this.buttonClick}>PRESS ME</button>
-          {this.state.show && <Snippet title="Hello" />}
-        </header>
-      </div>
+      <>
+        <Router>
+          <NavBar title="Strivestaurant" />
+          <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
+          <Route path="/reservation" exact component={Reservations} />
+          <Route path="/details/:id" component={DishDetail} />
+        </Router>
+      </>
     );
   }
 }
